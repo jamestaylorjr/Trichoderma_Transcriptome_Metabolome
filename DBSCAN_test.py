bd8c0b649ce2283c8307f0e6f559d7c3495f542b
James Taylor <jimtaylor@tamu.edu>
2018-10-30 16:01:47 -0500

    Integrated automatic import of output files

--- DBSCAN_test.py
+++ DBSCAN_test.py
@@ -8,23 +8,26 @@ import time
 plt.style.use('gadfly')
 plot_kwds = {'alpha' : 0.25, 's' : 80, 'linewidths':0}
 
-def plot_clusters(data, algorithm, args, kwds):
+def plot_clusters(datax,datay,subplot, algorithm, args, kwds):
     start_time = time.time()
-    labels = algorithm(*args, **kwds).fit_predict(data)
+    data_pred = pd.DataFrame([datax,datay]).transpose()
+    cnames = list(data_pred)
+    labels = algorithm(*args, **kwds).fit_predict(data_pred)
     end_time = time.time()
     palette = sns.color_palette('deep', np.unique(labels).max() + 1)
     colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in labels]
-    plt.scatter(data['6hr'], data['12hr'], c=colors, **plot_kwds)
+    plt.subplot(subplot)
+    plt.scatter(datax, datay, c=colors, **plot_kwds)
     frame = plt.gca()
     frame.axes.get_xaxis().set_visible(False)
     frame.axes.get_yaxis().set_visible(False)
-    plt.title('Clusters found by {}'.format(str(algorithm.__name__)), fontsize=10)
+    plt.title('{} vs {} clusters'.format(cnames[0],cnames[1]), fontsize=10)
     print('Clustering took {:.2f} s'.format(end_time - start_time))
     return labels
 
 # %%
-FILEPATH = "C:/Users/jimta/Desktop/fc_degs.txt"
-data = pd.read_csv(FILEPATH, names =['gene','6hr','12hr'])
+FILEPATH = "C:/Users/jimta/Desktop/fc_secreted_degs.txt"
+data = pd.read_csv(FILEPATH, names =['gene','6hr','12hr','15hr','24hr','36hr'])
 full_data = data
 data = data.drop(['gene'],axis=1)
 
@@ -34,16 +37,25 @@ frame.axes.get_xaxis().set_visible(True)
 frame.axes.get_yaxis().set_visible(True)
 
 # %%
-label = plot_clusters(data, DBSCAN, (), {'eps':0.37})
-full_data['group'] = label
+comparisons = [['6hr','12hr'],['12hr','15hr'],['15hr','24hr'],['24hr','36hr']]
+sp = 221
+clusters_by_comparison = []
+for comp in comparisons:
+    x,y = comp
+    label = plot_clusters(data[x],data[y],sp, DBSCAN,(),{'eps':.5})
+    full_data['{}vs{}'.format(x,y)] = label
+    groups = []
+    for i in np.unique(label):
+        g = [full_data['gene'][x] for x, v in enumerate(full_data['{}vs{}'.format(x,y)]) if v == i]
+        groups.append(g)
+    clusters_by_comparison.append(groups)
+    sp = sp+1
 
 # %%
-groups = []
-for i in np.unique(label):
-    g = [full_data['gene'][x] for x, v in enumerate(full_data['group']) if v == i]
-    groups.append(g)
 
-# %%
-group_df = pd.DataFrame(groups)
+group_df = pd.DataFrame(clusters_by_comparison)
 group_df = group_df.transpose()
-group_df.to_csv('C:/Users/jimta/Desktop/DBSCAN_groups_effectoronly.csv')
+group_df.columns = ['6v12','12v15','15v24','24v36']
+
+for comp in list(group_df):
+    group_df[comp].to_csv('C:/Users/jimta/Desktop/{}_DBSCAN_groups_secretedonly.csv'.format(comp))
